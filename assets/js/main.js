a64f607f0c9b1a80bd9395dcad27a1b3e1219a5e
task1 <96787662+prodhan482@users.noreply.github.com>
2023-10-02 01:23:08 +0600

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /**
 * Template Name: MyResume
-* Updated: Jul 27 2023 with Bootstrap v5.3.1
+* Updated: Mar 10 2023 with Bootstrap v5.2.3
 * Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
